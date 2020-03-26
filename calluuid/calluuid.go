15e7ccc97a8668d82084ee01315b348455d2f0bf
yachang <changya@gmail.com>
2020-03-25 23:05:27 -0400

    second

--- calluuid/calluuid.go
+++ calluuid/calluuid.go
@@ -1,11 +1,22 @@
 // package to send request to uuid IP service and unpack annotation results.
+package calluuid
 
-package uuidcaller
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"net"
 
-imports {
 	"github.com/m-lab/uuid-annotator/ipservice"
-}
+)
 
-func main() {
-   d, err := ioutil.TempDir("", "TestServerAndClientE2E")
+func CreateClient(ip string) {
+	d, err := ioutil.TempDir("", "TestServerAndClientE2E")
+	sock := d + "/annotator.sock"
+	c := ipservice.NewClient(sock)
+	ctx := context.Background()
+	got, err := c.Annotate(ctx, net.ParseIP(ip))
+
+	log.Println(got)
+	log.Println(err)
 }
